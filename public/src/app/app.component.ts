8967a68703bab24f72fe792170eb71c92f2d23b1
Greg PFISTER <no-reply@mail.com>
2022-01-15 10:39:58 +0100

    Add @angular/fire, fix eslint

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+// import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
-  selector: 'osk-root',
+  selector: 'osk-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class OSKAppComponent {
-  title = 'public';
+  title = 'Example';
+  // constructor(private dialog: MatDialog, private store: AngularFirestore) { }
 }
--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
+import { environment } from 'src/environments/environment';
 import { OSKAppRoutingModule } from './app-routing.module';
 import { OSKAppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,7 +22,12 @@ import { OSKUserComponent } from './user/user.component';
   imports: [
     BrowserModule,
     OSKAppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    provideFirebaseApp(() => initializeApp({ ...environment.firebaseConfig })),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
   ],
   providers: [],
   bootstrap: [OSKAppComponent]
